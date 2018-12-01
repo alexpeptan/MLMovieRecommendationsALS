ba4d84cc8ffaee3bbba056cc1dec6ade786939f2
Alexandru Peptan <alexpeptan@yahoo.com>
2018-12-02 01:41:21 +0200

    Cleanup and adding my notes on algorithms

--- Main.py
+++ Main.py
@@ -6,16 +6,16 @@ from scipy.sparse import coo_matrix
 datafile = 'resources/u.data'
 data = pd.read_csv(datafile, sep="\t", header=None, usecols=[0, 1, 2], names=['userId', 'itemId', 'rating'])
 
-# Construct Users vs Items matrix
+# Construct Users vs Movies rating matrix
 data['userId'] = data['userId'].astype("category")
 data['itemId'] = data['itemId'].astype("category")
 rating_matrix = coo_matrix((data['rating'].astype(float),
                             (data['itemId'].cat.codes.copy(),
                              data['userId'].cat.codes.copy())))
 
+
 # Training Phase
 
-# Deprecated
 # user_factors, item_factors = implicit.alternating_least_squares(rating_matrix, factors=10, regularization=0.01)
 # WARNING:implicit:This method is deprecated. Please use the AlternatingLeastSquares class instead
 
@@ -27,26 +27,21 @@ item_factors = als.item_factors
 
 # Testing Phase
 
-# Array of ratings for all the movies for user 196
-# user196=item_factors.dot(user_factors[196])
-# print(user196)
-
+# Returns top k most rated movies for user_id
 def topk_for_user(topk, user_id):
     user = item_factors.dot(user_factors[user_id])
 
     return heapq.nlargest(topk, range(len(user)), user.take)
 
 
-# Print top 5 recommended movies for user with id 196
 print(topk_for_user(5, 196))
-# Print top 3 recommended movies for user with id 007
 print(topk_for_user(3, 7))
 
-# TODO: See how I can make such that the recommendations will remain the same on rerunning the fitting algorithm.
-# It makes all the sense in the world for a recommendation algorithm to produce reproducible results
-# Or is the current dataset  "too relaxed" (factors=10, regularization=0.01)?
-# It allows for too much of a variety of solutions(too few factors?)
+# TODO: How can I stabilize the received recommendations upon repeated reruns of the fitting algorithm?
+# We should have reproducible results
+# Or does a model configured with (factors=10, regularization=0.01) not capture that complexity?
+# Does it allow for too many of solutions to exist? (too few factors/too small of a regularization constant?)
 
-# This configuration(factors=30, regularization=1e-9) appears to provide more stable/reproducible results.
+# This configuration(factors=30, regularization=1e-9) appears to provide more stable results.
 # Still far from perfect.
 # More analysis needed to better calibrate the ALS algorithm
